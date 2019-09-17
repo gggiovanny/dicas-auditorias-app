32e6080d9d4d227f31536b53723ae0a2267eaa25
gggiovanny <giovanny.gonzalez.19@gmail.com>
2019-09-17 18:02:24 -0500

    Cambiada la manera en la que visualizan los chips de activos de auditoria. Falta corregir un bug que hacen que se chequeen activos que no son

--- app/src/main/java/com/dicas/auditorias/ui/activos/RecyclerActivosAdapter.kt
+++ app/src/main/java/com/dicas/auditorias/ui/activos/RecyclerActivosAdapter.kt
@@ -1,7 +1,11 @@
 package com.dicas.auditorias.ui.activos
 
+
+import android.content.res.ColorStateList
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.databinding.library.baseAdapters.BR
@@ -12,6 +16,10 @@ import kotlinx.android.synthetic.main.layout_activo_item.view.*
 
 class RecyclerActivosAdapter(var viewModel: ActivosViewModel, var id_layout_item: Int) :
     RecyclerView.Adapter<RecyclerActivosAdapter.ActivoCardHolder>() {
+
+    companion object {
+        private const val TAG = "RecyclerActivosAdapter"
+    }
 
     var activos: List<Activo>? = null
 
@@ -62,16 +70,30 @@ class RecyclerActivosAdapter(var viewModel: ActivosViewModel, var id_layout_item
         }
 
         private fun setChipsExistenciasValue(activoActual: Activo) {
-            val trueChar: Char = '\u0001'
-            val falseChar: Char = '\u0000'
 
-            if (activoActual.existencia_guardada == trueChar.toString())
-                itemView.chip_existencia_guardada.text =
-                    itemView.context.getText(R.string.activo_guardado_si)
-            else {
-                if (!activoActual.existencia_guardada.isNullOrEmpty())
-                    itemView.chip_existencia_guardada.text =
-                        itemView.context.getText(R.string.activo_guardado_no)
+            if (activoActual.id == "65") {
+                Log.d(TAG, "setChipsExistenciasValue: anuma")
+            }
+
+            if (activoActual.existencia_guardada == "1") {
+                itemView.chip_existencia_guardada.chipIcon =
+                    itemView.context.getDrawable(R.drawable.ic_check_box_true_black_24dp)
+                itemView.chip_existencia_guardada.chipIconTint = ColorStateList.valueOf(
+                    ContextCompat.getColor(
+                        itemView.context,
+                        R.color.sucess_green
+                    )
+                )
+            }
+            if (activoActual.existencia_guardada == "0") {
+                itemView.chip_existencia_guardada.chipIcon =
+                    itemView.context.getDrawable(R.drawable.ic_check_box_false_blank_black_24dp)
+                itemView.chip_existencia_guardada.chipIconTint = ColorStateList.valueOf(
+                    ContextCompat.getColor(
+                        itemView.context,
+                        R.color.error_red
+                    )
+                )
             }
 
         }
